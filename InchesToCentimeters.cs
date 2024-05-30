f94ca125654c0fc12f05d4c6b5b61cebd3ca15e7
musa islam <hamerofth@gmail.com>
2024-05-30 09:11:13 -0400

    changes

---
+++ InchesToCentimeters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+class InchesToCentimeters
+{
+    static void Main()
+    {
+        const double cpi = 2.54;
+        Write("Enter the measurement in inches: ");
+        double inches = Convert.ToDouble(ReadLine());
+        double centimeters = inches * cpi;
+        WriteLine($"{inches} inches is as big as {centimeters} centimeters.");
+    }
+}
+
+
+
+
+

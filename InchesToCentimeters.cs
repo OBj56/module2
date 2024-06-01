99f8669a1e188a1ae49f1503084c6d49dbaf8e93
musa islam <hamerofth@gmail.com>
2024-06-01 16:25:49 -0400

    new21

--- InchesToCentimeters.cs
+++ InchesToCentimeters.cs
@@ -5,10 +5,10 @@ class InchesToCentimeters
     static void Main()
     {
         const double cpi = 2.54;
-        double inches = 5;
+        double inches = 15994;
 
         double centimeters = inches * cpi;
 
       WriteLine(inches + " inches is " + centimeters + " centimeters.");
     }
-}
+} 
